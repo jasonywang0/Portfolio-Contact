1c0bec25b351bfe37dceda35ed89c40c7ab545d4
Jason Wang <jasonywang0@gmail.com>
2021-06-15 22:40:55 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.SENDER_PASSWORD,
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+
+router.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const subject = req.body.subject; 
+  const message = req.body.message; 
+  const mail = {
+    from: email,
+    to: "jasonywang0@gmail.com",
+    subject: subject,
+    html: `
+            <p>Sent from portfolio website</p>
+            <p>Name: ${name}</p>
+            <p>Email: ${email}</p>
+            <p>Message: ${message}</p>`,
+  };
+
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "SENT" });
+    }
+  });
+});
