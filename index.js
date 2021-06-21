b2557fb607ce70764ae4c475cfeaa4981b582237
jasonywang0 <jasonywang0@gmail.com>
2021-06-20 20:20:24 -0700

    moved cors up

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const limiter = rateLimit({
 });
 
 const app = express();
+app.use(cors());
 app.set('trust proxy', 1);
 app.use(limiter);
-app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(process.env.PORT || 5000, () => console.log("Server Running"));
